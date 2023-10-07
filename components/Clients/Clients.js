7315be3854298db0deafb20b3534166829fd05b5
hkmr <harshkmr991234@gmail.com>
2023-10-07 18:04:03 +0530

    Work Section updated

--- components/Clients/Clients.js
+++ components/Clients/Clients.js
@@ -13,10 +13,10 @@ import { raleway_display } from "@/app/fonts";
 const Clients = () => {
   const data = [
     {
-      title: "Decoras Furniture",
+      title: "Decoras Furniture ABSJASNKJS",
       client: "DECORASUSA.COM",
-      timeline: "JULY 2021",
-      role: "BRANDING, UX/UI, DEV",
+      timeline: "2021",
+      role: "Website Design",
       image: DecorasDesktop,
       imageMobile: DecorasMobile,
       link: "/",
@@ -24,7 +24,7 @@ const Clients = () => {
     {
       title: "Baazi Games",
       client: "DECORASUSA.COM",
-      timeline: "JULY 2021",
+      timeline: "2021",
       role: "BRANDING, UX/UI, DEV",
       image: BaziDesktop,
       imageMobile: BaziMobile,
@@ -33,7 +33,7 @@ const Clients = () => {
     {
       title: "Taxila Stone",
       client: "DECORASUSA.COM",
-      timeline: "JULY 2021",
+      timeline: "2021",
       role: "BRANDING, UX/UI, DEV",
       image: TaxilaDesktop,
       imageMobile: TaxilaMobile,
@@ -70,12 +70,14 @@ const Clients = () => {
 
       <div className={style.clientContainerMobile}>
         <div className={style.work_count_mobile}>
-          <p> {"/*"} WORK</p> <p>{/* 0 {index + 1} / 0{data.length} */}01/05</p>
+          <p> {"/*"} WORK</p>
         </div>
         <div className={style.carousalContainer}>
           {data.map((item, index) => {
             return (
-              <div key={index} className={style.singleClientMobile}>
+              <div key={index} >
+                <h2 className={style.workDateMobile}>01/05</h2>
+                <div key={index} className={style.singleClientMobile}>
                 
                 <div className={style.singleClientImageMobile}>
                   <Image src={item.image} alt={item.image} />
@@ -87,7 +89,7 @@ const Clients = () => {
                   <div className={style.singleClientTextContainerMobile}>
                     <div>
                       <p>{item.client}</p>
-                      <p className="singleClientTextRoleTextMobile">{item.role} . {item.timeline}</p>
+                      <p className={style.singleClientTextRoleTextMobile}>{item.role} . {item.timeline}</p>
                       <p></p>
                     </div>
 
@@ -100,8 +102,15 @@ const Clients = () => {
                   </div>
                 </div>
               </div>
+              </div>
             );
           })}
+
+          <Button link="#" text="See All Works" className={style.allWorkButton}>
+            <svg width="25" height="20" viewBox="0 0 25 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M15.1724 1C15.1724 1 15.9326 5.43128 17.7931 7.32813C19.6536 9.22497 24 10 24 10M24 10C24 10 19.6536 10.775 17.7931 12.6719C15.9326 14.5687 15.1724 19 15.1724 19M24 10L0 10" stroke="#F0F0F0"/>
+            </svg>
+          </Button>
         </div>
       </div>
     </>
