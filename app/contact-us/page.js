f1e674db6051cbd14bb4eacaa8e9c7c4117971e2
Ajay Kumar Upadhyay <ajaykumarupadhyay@Ajays-MacBook-Air.local>
2023-12-16 19:04:28 +0530

    build fi

--- app/contact-us/page.js
+++ app/contact-us/page.js
@@ -1,10 +1,16 @@
 import React from "react";
 import LetsTalk from "../../components/LetsTalk/LetsTalk";
+
+import CursorContextProvider from "../../components/cursourAnimation/cursorContext";
+import Cursor from "../../components/cursourAnimation/cursor";
 const Contact = () => {
   return (
-    <div className="app_container" style={{marginTop:"var(--padding)"}}>
-      <LetsTalk />
-    </div>
+    <CursorContextProvider>
+      <Cursor />
+      <div className="app_container" style={{ marginTop: "var(--padding)" }}>
+        <LetsTalk />
+      </div>
+    </CursorContextProvider>
   );
 };
 
