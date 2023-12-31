faf275630741607cc4a026bb61ca89335103db8f
Ajay Kumar Upadhyay <ajaykumarupadhyay@Ajays-MacBook-Air.local>
2023-12-31 12:53:02 +0530

    scroll optimize

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 "use client";
-
-import React, { useState, useEffect, createContext, useContext } from "react";
 import {
   Banner,
   Clients,
@@ -11,34 +9,13 @@ import {
   // Testimonial,
 } from "../components";
 
-import CursorContextProvider from "../components/cursourAnimation/cursorContext";
+
 import Cursor from "../components/cursourAnimation/cursor";
 export default function Home() {
-  const [scrollTop, setScrollTop] = useState(0);
-
-  const onScroll = () => {
-    const winScroll = document.documentElement.scrollTop;
-    const height =
-      document.documentElement.scrollHeight -
-      document.documentElement.clientHeight;
-    const scrolled = (winScroll / height) * 100;
-
-    setScrollTop(scrolled);
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", onScroll);
-    // return () => window.removeEventListener("scroll", onScroll);
-  }, []);
   return (
     <>
       <Cursor />
-      <div className="progressMainWrapper">
-        <div
-          className="progressMainStyle"
-          style={{ width: `${scrollTop}%` }}
-        ></div>
-      </div>
+      <div className="progressMainWrapper"></div>
       <div className="app_container">
         <Banner />
         <Clients />
